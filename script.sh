6e3085b0f9ed0825dd4eb9d7a4920727d8dc9fba
Rahul Ramesh <rahul13ramesh@gmail.com>
2022-11-27 13:01:03 +0000

    simclr

--- script.sh
+++ script.sh
@@ -1,23 +1,30 @@
-python3 main.py --tag t1 --data_classes "[1,2]" --seed 0
-python3 main.py --tag t1 --data_classes "[1,2]" --seed 10
-python3 main.py --tag t1 --data_classes "[1,2]" --seed 20
-python3 main.py --tag t1 --data_classes "[1,2]" --seed 30
-python3 main.py --tag t1 --data_classes "[1,2]" --seed 40
+python3 main.py --tag t1_n --data_classes "[0,1]" --seed 0
+python3 main.py --tag t1_n --data_classes "[0,1]" --seed 10
+python3 main.py --tag t1_n --data_classes "[0,1]" --seed 20
+python3 main.py --tag t1_n --data_classes "[0,1]" --seed 30
+python3 main.py --tag t1_n --data_classes "[0,1]" --seed 40
 notify "t1"
 
-python3 main.py --tag t2 --data_classes "[2,3]" --seed 0
-python3 main.py --tag t2 --data_classes "[2,3]" --seed 10
-python3 main.py --tag t2 --data_classes "[2,3]" --seed 20
-python3 main.py --tag t2 --data_classes "[2,3]" --seed 30
-python3 main.py --tag t2 --data_classes "[2,3]" --seed 40
-notify "t2"
+python3 main.py --tag all --data_classes "[0,1,2,3,4,5,6,7,8,9]" --seed 0 --epochs 500
+python3 main.py --tag all --data_classes "[0,1,2,3,4,5,6,7,8,9]" --seed 10 --epochs 500
+python3 main.py --tag all --data_classes "[0,1,2,3,4,5,6,7,8,9]" --seed 20 --epochs 500
+python3 main.py --tag all --data_classes "[0,1,2,3,4,5,6,7,8,9]" --seed 30 --epochs 500
+python3 main.py --tag all --data_classes "[0,1,2,3,4,5,6,7,8,9]" --seed 40 --epochs 500
+notify "all"
+
+# python3 main.py --tag t2 --data_classes "[2,3]" --seed 0
+# python3 main.py --tag t2 --data_classes "[2,3]" --seed 10
+# python3 main.py --tag t2 --data_classes "[2,3]" --seed 20
+# python3 main.py --tag t2 --data_classes "[2,3]" --seed 30
+# python3 main.py --tag t2 --data_classes "[2,3]" --seed 40
+# notify "t2"
 
 
-python3 main.py --tag t5 --data_classes "[8,9]" --seed 0
-python3 main.py --tag t5 --data_classes "[8,9]" --seed 10
-python3 main.py --tag t5 --data_classes "[8,9]" --seed 20
-python3 main.py --tag t5 --data_classes "[8,9]" --seed 30
-python3 main.py --tag t5 --data_classes "[8,9]" --seed 40
-notify "t5"
+# python3 main.py --tag t5 --data_classes "[8,9]" --seed 0
+# python3 main.py --tag t5 --data_classes "[8,9]" --seed 10
+# python3 main.py --tag t5 --data_classes "[8,9]" --seed 20
+# python3 main.py --tag t5 --data_classes "[8,9]" --seed 30
+# python3 main.py --tag t5 --data_classes "[8,9]" --seed 40
+# notify "t5"
 
 
